341c59970d366708c990699f32d01aa4775295b0
Poh Keong <32123454+braveducky@users.noreply.github.com>
2018-11-18 20:00:00 +0800

    Delete hello.adb

--- src/main/src/hello.adb
+++
@@ -1,7 +0,0 @@
-with ada.text_io;   -- Tell compiler to use i/o library
-use  ada.text_io;   -- Use library routines w/o fully qualified names
-
-procedure hello is
-begin
-   put("Hi 320");
-end hello;
